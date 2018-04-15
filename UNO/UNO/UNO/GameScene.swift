fb5014d91fbb6fe745c7aeb51cb939e78946193a
Zach Schlotman <znet@Zachs-iMac.local>
2018-04-15 11:47:15 -0500

    Worked on Shuffle and stuff

--- UNO/UNO/UNO/GameScene.swift
+++ UNO/UNO/UNO/GameScene.swift
@@ -31,14 +31,17 @@ class GameScene: SKScene {
     struct Stack {
         //make the array properties part of stack, use.array for array functionality
         fileprivate var array: [Card] = []
+        var count : Int = 0
         //addcards to the last position of the array
         mutating func push(_ element:Card)
         {
             array.append(element)
+            count+=1
         }
         //take the last card from the array removing it and returning it
         mutating func pop() -> Card?
         {
+            count-=1
             return array.popLast()
         }
         //returns the last card in the array
@@ -47,14 +50,14 @@ class GameScene: SKScene {
         }
     }
     //players play their card and place here
-    var pool = Stack()
+    var pool : [Card] = []
     //players draw cards from
-    var drawPile = Stack()
+    var drawPile : [Card] = []
     //NOTE THIS IS NOT GOING TO STAY
     
     //all Cards initalized here (IK hard code is bad but i get nullPointer issues. If you figure a better way thats great)
         //red
-        let red1 = Card(clr: .red, typ: .normal, num: 1)
+        /*let red1 = Card(clr: .red, typ: .normal, num: 1)
         let red2 = Card(clr: .red, typ: .normal, num: 2)
         let red3 = Card(clr: .red, typ: .normal, num: 3)
         let red4 = Card(clr: .red, typ: .normal, num: 4)
@@ -98,7 +101,23 @@ class GameScene: SKScene {
         let yellow9 = Card(clr: .red, typ: .normal, num: 9)
         let yellow10 = Card(clr: .red, typ: .normal, num: 10)
     
-    
+        */
+    func shuffle( deck: [Card])-> [Card]{
+        var i = deck.count-1
+        var d = deck
+        while(i >= 1){
+
+            let r = Int(arc4random()) % (i)
+            let sv : Card = deck[i]
+            d[i] = deck[r]
+            d[r] = sv
+            
+            i-=1
+        }
+        return d
+    }
+    var iDeck : [Card] = []
+    var iDecks  = Stack()
     //takes all cards and assigns a location for them
     func fillDecks()
     {
@@ -107,8 +126,28 @@ class GameScene: SKScene {
         //addChild(CARD) activates the node to be used by the game handler
         
         //NOTE @CARDS(BLUE & YELLOW STILL NEED TEMPORARY POSITIONS)
-        
-        drawPile.push(red1)
+        var i = 0
+        while(i < 11){
+            iDeck.append(Card(clr:.red,typ:.normal,num:i))
+            iDeck.append(Card(clr:.yellow,typ:.normal,num:i))
+            iDeck.append(Card(clr:.blue,typ:.normal,num:i))
+            iDeck.append(Card(clr:.green,typ:.normal,num:i))
+            i+=1
+        }
+        iDeck = shuffle(deck : iDeck)
+        for var c : Card in iDeck{
+            iDecks.push(c)
+        }
+        i = 0
+        while(i < 7){
+            playerDeck.append(iDecks.pop()!)
+            computerDeck.append(iDecks.pop()!)
+            i+=1
+        }
+        while(iDecks.count > 0){
+            drawPile.append(iDecks.pop()!)
+        }
+        /*drawPile.push(red1)
         red1.position = CGPoint(x: 100, y: 200)
         addChild(red1)
         drawPile.push(red2)
@@ -210,10 +249,10 @@ class GameScene: SKScene {
         drawPile.push(yellow9)
         addChild(yellow9)
         drawPile.push(yellow10)
-        addChild(yellow10)
+        addChild(yellow10)*/
         
         //divide into intial groups of cards
-        var i : Int = 1
+        i = 1
         while i <= 10 {
             playerDraw()
             computerDraw()
@@ -223,12 +262,14 @@ class GameScene: SKScene {
     //player takes a card from the draw pile
     func playerDraw()
     {
-        playerDeck.append(drawPile.pop()!)
+        playerDeck.append(drawPile[drawPile.count-1])
+        drawPile.remove(at: drawPile.count-1)
     }
     //computer takes a card from the draw pile
     func computerDraw()
     {
-        computerDeck.append(drawPile.pop()!)
+        computerDeck.append(drawPile[drawPile.count-1])
+        drawPile.remove(at: drawPile.count-1)
     }
     //checks to see if either the player or cpu has meet the parameters to win
     func checkDeckSize()
